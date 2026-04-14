68ca5a65529c6936ca4e21cc42138ac0a9b08ebc
AyoubKhaoua <mr.ayoubkhaoua@example.com>
2026-04-14 12:48:30 +0200

    create controller for parte profile

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class ProfileController extends Controller {}
